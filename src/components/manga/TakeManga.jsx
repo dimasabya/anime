43841262dba6c6d577e2a8087256ed8d5cbad699
dimasabya <dimasabdulyasir9@gmail.com>
2024-01-28 13:21:40 +0700

    update TakeManga flex

--- src/components/manga/TakeManga.jsx
+++ src/components/manga/TakeManga.jsx
@@ -100,7 +100,7 @@ const TakeManga = () => {
                 <p className="text-gray-400 text-[.9rem] my-4">Source: {manga.source}</p>
                 <div className="">
                   <p>Tags:</p>
-                  <div className="flex gap-2 my-2 ml-2">
+                  <div className="flex flex-wrap gap-2 my-2 ml-2">
                     {manga.genres.map((gen, i) => (
                       <p key={i} className="border p-1 px-2">
                         {gen.name}
