0239eb00a78c5dc36b86a2debba901ba3701b8fa
dimasabya <dimasabdulyasir9@gmail.com>
2024-01-28 14:01:42 +0700

    update MangaPopuler grid isLoad

--- src/components/populers/manga/MangaPopuler.jsx
+++ src/components/populers/manga/MangaPopuler.jsx
@@ -108,54 +108,52 @@ const MangaPopuler = () => {
         </div>
       </div>
       <h1 className="text-2xl ml-2">Popular</h1>
-      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 text-center">
-        {isLoad ? (
-          <>
-            {mangas.map((manga, i) => (
-              <NavLink key={i} to={`populer/req?id=${manga.mal_id}`}>
-                <div className="p-2 relative" onMouseEnter={() => handleInfo(i)} onMouseLeave={() => handleHidenInfo()}>
-                  <img
-                    src={manga.images.jpg.image_url}
-                    alt=""
-                    className="w-[125px] md:w-[250px] h-[135px] md:h-[250px]"
-                  />
-                  <h3 className="mt-2 text-[.75rem] md:text-lg">{manga.title.substring(0, 25)}</h3>
-                  {active === i && (
-                    <div className="bg-gray-500 opacity-[.95] absolute top-0 bottom-0 left-0 right-0 text-start p-2 rounded-lg">
-                      <div className="">
-                        <div>
-                          <h3 className="font-bold md:mb-2 text-[.9rem] md:text-lg">{manga.title.substring(0, 25)}</h3>
+      {isLoad ? (
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-4 text-center">
+          {mangas.map((manga, i) => (
+            <NavLink key={i} to={`populer/req?id=${manga.mal_id}`}>
+              <div className="p-2 relative" onMouseEnter={() => handleInfo(i)} onMouseLeave={() => handleHidenInfo()}>
+                <img
+                  src={manga.images.jpg.image_url}
+                  alt=""
+                  className="w-[125px] md:w-[250px] h-[135px] md:h-[250px]"
+                />
+                <h3 className="mt-2 text-[.75rem] md:text-lg">{manga.title.substring(0, 25)}</h3>
+                {active === i && (
+                  <div className="bg-gray-500 opacity-[.95] absolute top-0 bottom-0 left-0 right-0 text-start p-2 rounded-lg">
+                    <div className="">
+                      <div>
+                        <h3 className="font-bold md:mb-2 text-[.9rem] md:text-lg">{manga.title.substring(0, 25)}</h3>
+                        <p className="text-[.5rem] md:text-[.85rem]">
+                          <i className="fa-solid fa-star"></i> {manga.score}{" "}
+                          <i className="fa-solid fa-ranking-star"></i> {manga.rank}
+                        </p>
+                        <p className="text-[.5rem] md:text-[.85rem]">
+                          <i className="fa-regular fa-file-lines"></i> {manga.chapters ? manga.chapters : 0} Chapters
+                        </p>
+                        <div className="inline-flex gap-2">
+                          <p className="text-[.5rem] md:text-[.85rem] italic">{manga.status}</p>
                           <p className="text-[.5rem] md:text-[.85rem]">
-                            <i className="fa-solid fa-star"></i> {manga.score}{" "}
-                            <i className="fa-solid fa-ranking-star"></i> {manga.rank}
+                            <i className="fa-solid fa-calendar-days"></i> {manga.published.prop.from.year}
                           </p>
-                          <p className="text-[.5rem] md:text-[.85rem]">
-                            <i className="fa-regular fa-file-lines"></i> {manga.chapters ? manga.chapters : 0} Chapters
-                          </p>
-                          <div className="inline-flex gap-2">
-                            <p className="text-[.5rem] md:text-[.85rem] italic">{manga.status}</p>
-                            <p className="text-[.5rem] md:text-[.85rem]">
-                              <i className="fa-solid fa-calendar-days"></i> {manga.published.prop.from.year}
-                            </p>
-                          </div>
-                          <p className="my-2 text-[.5rem] md:text-[.9em]">{manga?.synopsis?.substring(0, 100)}</p>
                         </div>
-                        <div className="absolute bottom-2 left-0 right-0 text-center">
-                          <i className="fa-solid fa-play md:text-2xl"></i>
-                          <i className="fa-solid fa-bookmark mx-8 md:text-2xl z-[99]"></i>
-                          <i className="fa-solid fa-plus md:text-2xl"></i>
-                        </div>
+                        <p className="my-2 text-[.5rem] md:text-[.9em]">{manga?.synopsis?.substring(0, 100)}</p>
+                      </div>
+                      <div className="absolute bottom-2 left-0 right-0 text-center">
+                        <i className="fa-solid fa-play md:text-2xl"></i>
+                        <i className="fa-solid fa-bookmark mx-8 md:text-2xl z-[99]"></i>
+                        <i className="fa-solid fa-plus md:text-2xl"></i>
                       </div>
                     </div>
-                  )}
-                </div>
-              </NavLink>
-            ))}
-          </>
-        ) : (
-          <Loading />
-        )}
-      </div>
+                  </div>
+                )}
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
